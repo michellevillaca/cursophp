252c19eee8e19c81e947e2192b5d9679ff234539
michellevillaca <michellevandry@gmail.com>
2025-03-23 18:55:30 -0300

    Exercício de strings
    -Diferenciação de double quoted e single quoted

---
+++ ex004/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Manipulação de Strings</title>
+</head>
+<body>
+    <h1>Manipulação de Strings</h1>
+    <?php 
+        $nome = "Michelle";
+        echo "Olá, $nome! \u{1F920} <br>";
+        echo 'Olá, $nome! \u{1F920}';
+    ?>
+</body>
+</html>
