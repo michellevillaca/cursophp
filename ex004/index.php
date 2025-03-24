97cb20b975a7c1b3f2e2df1a3d6ad7431b430f7f
michellevillaca <michellevandry@gmail.com>
2025-03-24 14:57:40 -0300

    Atualização com exemplo de constante

--- ex004/index.php
+++ ex004/index.php
@@ -8,9 +8,16 @@
 <body>
     <h1>Manipulação de Strings</h1>
     <?php 
+        //para variáveis:
         $nome = "Michelle";
-        echo "Olá, $nome! \u{1F920} <br>";
-        echo 'Olá, $nome! \u{1F920}';
+        echo "Olá, $nome! \u{1F920} <br>"; //aspas duplas --> ocorre a interpolação;
+        echo 'Olá, $nome! \u{1F920} <br>'; //aspas simples --> não ocorre a interpolação.
+
+        //para constantes:
+        const ESTADO = "São Paulo";
+        echo "Eu moro em ESTADO. <br>";
+        echo 'Eu moro em ESTADO. <br>';
+        echo "Eu moro em " .ESTADO; //a interpretação do dado da constante só ocorrerá dessa forma, com o operador de concatenação.
     ?>
 </body>
 </html>
