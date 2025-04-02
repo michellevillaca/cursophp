7a1fcbf7177142e293de24ec586ddb6e19275d36
Michelle Villaça Andry <michellevandry@gmail.com>
2025-04-02 15:14:23 -0300

    Configurações de style no PHP

--- desafios/d001/num.php
+++ desafios/d001/num.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Desafio 1 - Informe um Número</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
     <section>
@@ -14,6 +15,7 @@
             $suc = $num + 1;
             echo "<p>O número escolhido foi $num.<br>O seu antecessor é $ant.<br>O seu sucessor é $suc."
         ?>
+         <p><button type="button"><a href="javascript:history.go(-1)">Voltar para a página anterior.</a></button></p>
     </section>
 </body>
 </html>
