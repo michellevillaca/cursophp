3a82e7d0f1f7e0ae9dab0e23ed9be7b024ab8b2d
Michelle Villaça Andry <michellevandry@gmail.com>
2025-04-02 20:54:34 -0300

    Update na função (mt_rand)

--- desafios/d002/index.php
+++ desafios/d002/index.php
@@ -11,7 +11,7 @@
         <h1>Trabalhando com números aleatórios</h1>
         <p>Gerando um número aleatório entre 0 e 100...</p>
         <?php
-            $num = rand(0,100);
+            $num = mt_rand(0,100);
             echo "<p>O valor gerado foi <strong>$num</strong>.</p>";
         ?>
         <button onclick="javascript:window.location.href='index.php'">&#x1F504; Gerar novamente</button>
