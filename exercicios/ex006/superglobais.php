1065d0d09ac5c0594b5a909a4f6c38f0c27e2f13
Michelle Villaça Andry <michellevandry@gmail.com>
2025-04-10 20:56:18 -0300

    Adicionando superglobais

--- exercicios/ex006/superglobais.php
+++ exercicios/ex006/superglobais.php
@@ -10,11 +10,28 @@
     <main>
         <pre>
             <?php 
+                setcookie("dia-da-semana", "SEGUNDA", time() + 3600);
+                
+                session_start();
+                $_SESSION["teste"] = "FUNCIONOU!";
+
                 echo "<h1>Superglobal GET</h1>";
                 var_dump($_GET);
 
                 echo "<h1>Superglobal POST</h1>";
                 var_dump($_POST);
+
+                echo "<h1>Superglobal REQUEST</h1>";
+                var_dump($_REQUEST);
+
+                echo "<h1>Superglobal COOKIE</h1>";
+                var_dump($_COOKIE);
+
+                echo "<h1>Superglobal SESSION</h1>";
+                var_dump($_SESSION);
+
+                echo "<h1>Superglobal ENV</h1>";
+                var_dump($_ENV);
             ?>
         </pre>
     </main>
