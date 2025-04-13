1e18cdecb19ebe22e51170c61c349af46e96527a
Michelle Villaça Andry <michellevandry@gmail.com>
2025-04-13 18:33:42 -0300

    Conclusão do formulário retroalimentado

--- exercicios/ex007/index.php
+++ exercicios/ex007/index.php
@@ -8,16 +8,27 @@
 </head>
 <body>
     <?php 
-    
+        //Capturando os dados do furmulário retroalimentado
+        $valor1 = $_GET['v1']??0;
+        $valor2 = $_GET['v2']??0;
+
     ?>
     <main>
-        <form action="<?php echo $_SERVER['PHP_SELF'] ?>" method="get">
+        <form action="<?= $_SERVER['PHP_SELF'] ?>" method="get">
             <label for="v1">Valor 1</label>
-            <input type="number" name="v1" id="v1">
+            <input type="number" name="v1" id="v1" value="<?=$valor1?>">
             <label for="v2">Valor 2</label>
-            <input type="number" name="v2" id="v2">
+            <input type="number" name="v2" id="v2" value="<?=$valor2?>">
             <input type="submit" value="Somar">
         </form>
     </main>
+
+    <section id="resultado">
+        <h2>Resultado da Soma</h2>
+        <?php 
+            $soma = $valor1 + $valor2;
+            echo "<p>A soma entre os valores $valor1 e $valor2 é <strong>igual a $soma</strong>.</p>"
+        ?>
+    </section>
 </body>
 </html>
