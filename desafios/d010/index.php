956295aa97af8a38ecdf3052fcc9ad1a8782bfaf
Michelle Villaça Andry <michellevandry@gmail.com>
2025-04-18 13:05:58 -0300

    Criação (início) desafio 11 - reajustando preços

--- desafios/d010/index.php
+++ desafios/d010/index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Desafio 9 - Médias Aritméticas</title>
+    <title>Desafio 10 - Cálculo de Idade</title>
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
---
+++ desafios/d011/index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Desafio 11 - Reajustando Preços</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <?php 
+        $preço = $_GET['preço']??1;
+        $reajuste = $_GET['reajuste']??25;
+    ?>
+
+    <main>
+        <h1>Reajustador de Preços</h1>
+        <form action="<?=$_SERVER['PHP_SELF']?>" method="get">
+            <label for="preço">Preço do Produto (R$)</label>
+            <input type="number" name="preço" id="preço">
+            <label for="reajuste">Qual será o percentual de reajuste?</label>
+            <input type="range" name="reajuste" id="reajuste">
+            <input type="submit" value="Reajustar">
+        </form>
+    </main>
+
+    <section id="resultado">
+        <h2>Resultado do Reajuste</h2>
+        
+    </section>
+</body>
+</html>
