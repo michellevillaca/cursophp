dfb622cea85eea01cf1300656974aa7787cf72f0
Michelle Villaça Andry <michellevandry@gmail.com>
2025-04-18 13:15:38 -0300

    update min e max range

--- desafios/d011/index.php
+++ desafios/d011/index.php
@@ -18,14 +18,13 @@
             <label for="preço">Preço do Produto (R$)</label>
             <input type="number" name="preço" id="preço">
             <label for="reajuste">Qual será o percentual de reajuste?</label>
-            <input type="range" name="reajuste" id="reajuste">
+            <input type="range" name="reajuste" id="reajuste" min="0" max="100" value="50">
             <input type="submit" value="Reajustar">
         </form>
     </main>
 
     <section id="resultado">
         <h2>Resultado do Reajuste</h2>
-        
     </section>
 </body>
 </html>
