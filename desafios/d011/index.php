22754612c05b72f661b814827b71a19e829a9aa7
Michelle Villaça Andry <michellevandry@gmail.com>
2025-04-18 15:12:18 -0300

    update range - mostrando a porcentagem

--- desafios/d011/index.php
+++ desafios/d011/index.php
@@ -9,22 +9,35 @@
 <body>
     <?php 
         $preço = $_GET['preço']??1;
-        $reajuste = $_GET['reajuste']??25;
+        $porcentagem = $_GET['reajuste']??25;
+        $fórmula = ($preço*$porcentagem) / 100;
     ?>
 
     <main>
         <h1>Reajustador de Preços</h1>
         <form action="<?=$_SERVER['PHP_SELF']?>" method="get">
             <label for="preço">Preço do Produto (R$)</label>
-            <input type="number" name="preço" id="preço">
-            <label for="reajuste">Qual será o percentual de reajuste?</label>
-            <input type="range" name="reajuste" id="reajuste" min="0" max="100" value="50">
+            <input type="number" name="preço" id="preço" min="0.10" step="0.01">
+
+            <label for="reajuste">Qual será o percentual de reajuste? (<strong><span id="porcentagem">50%</span></strong>)</label>
+            <input type="range" name="reajuste" id="reajuste" min="0" max="100" step="1" value="50">
+
             <input type="submit" value="Reajustar">
         </form>
     </main>
 
     <section id="resultado">
         <h2>Resultado do Reajuste</h2>
+        <p>O produto que custava R$[??], com <strong>[??] de aumento</strong> vai passar a custar <strong>R$[??]</strong> a partir de agora.</p>
     </section>
+
+    <script>
+        const range = document.getElementById("reajuste");
+        const porcentagem = document.getElementById("porcentagem");
+
+        range.addEventListener("input", function() {
+            porcentagem.textContent = range.value + "%";
+        })
+    </script>
 </body>
 </html>
