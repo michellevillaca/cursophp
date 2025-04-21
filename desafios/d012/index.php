131bfe14278922bc05f9cb0f78b1473628d35bcb
Michelle Villaça Andry <michellevandry@gmail.com>
2025-04-21 13:25:21 -0300

    Começo da estrutura do desafio 12

---
+++ desafios/d012/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Desafio 12 - Calculadora de Tempo</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <main>
+        <h1>Calculadora de Tempo</h1>
+        <form action="<?=$_SERVER['PHP_SELF']?>" method="get">
+            
+        </form>
+    </main>
+</body>
+</html>
